2821d024c7fb1df12c857ab961b4107c4d496f78
MoonWireCEO <andrew@moonwire.app>
2025-06-18 08:12:57 -0400

    Update main.py
    chore(main): register feedback_prediction_router for live prediction access

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from src.threshold_simulator_router import router as threshold_simulator_router
 from src.feedback_volatility_router import router as feedback_volatility_router
 from src.training_pair_router import router as training_pair_router
 from src.model_training_router import router as model_training_router
+from src.feedback_prediction_router import router as feedback_prediction_router
 
 app = FastAPI()
 
@@ -48,6 +49,7 @@ app.include_router(threshold_simulator_router)
 app.include_router(feedback_volatility_router)
 app.include_router(training_pair_router)
 app.include_router(model_training_router)
+app.include_router(feedback_prediction_router)
 
 @app.head("/ping", include_in_schema=False)
 async def ping_head():
