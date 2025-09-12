4c63720960e25663f5eec644c2fb110bba79f706
MoonWireCEO <andrew@moonwire.app>
2025-09-12 16:08:09 -0400

    Update mw_demo_summary.py

--- scripts/mw_demo_summary.py
+++ scripts/mw_demo_summary.py
@@ -39,7 +39,7 @@ from scripts.summary_sections import drift_check_section
 from scripts.summary_sections import drift_aware_inference_section
 from scripts.summary_sections import per_origin_thresholds_section
 from scripts.summary_sections import calibration_section
-
+from scripts.summary_sections import training_metadata_section
 
 
 
