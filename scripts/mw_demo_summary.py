206f858a78af4497d352c42855cfb45b4d1f2644
MoonWireCEO <andrew@moonwire.app>
2025-09-12 19:27:06 -0400

    Update mw_demo_summary.py

--- scripts/mw_demo_summary.py
+++ scripts/mw_demo_summary.py
@@ -44,7 +44,7 @@ from scripts.summary_sections import retrain_summary_section
 from scripts.summary_sections import training_data_snapshot_section
 from scripts.summary_sections import accuracy_by_version_section
 from scripts.summary_sections import rolling_accuracy_section
-
+from scripts.summary_sections import label_feedback_section
 
 
 
